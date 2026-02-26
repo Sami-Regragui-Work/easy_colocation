9f72e58848a9a3bb0d0baab11764125d9588148a
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-02-26 14:45:35 +0000

    feat(models): added Colocation model

---
+++ app/Models/Colocation.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Colocation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'owner_id', 'status'];
+
+    protected function casts(): array
+    {
+        return [
+            'status' => 'string'
+        ];
+    }
+}
