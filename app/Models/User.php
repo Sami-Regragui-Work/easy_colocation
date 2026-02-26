3988f1a1ad483eaaa0430efbb9490ffa9e9b44be
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-02-26 09:13:32 +0000

    feat(models): updated User model fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin',
+        'is_banned',
+        'reputation'
     ];
 
     /**
@@ -43,6 +46,8 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_admin' => 'boolean',
+            'is_banned' => 'boolean'
         ];
     }
 }
