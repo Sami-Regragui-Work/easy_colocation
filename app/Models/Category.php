582f09ef8f1d77826de76bc5122822014fe9a19d
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-02-27 08:44:31 +0000

    feat(models): added Category relations

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,4 +11,14 @@ class Category extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = ['name', 'colocation_id'];
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function expenses()
+    {
+        return $this->hasMany(Expense::class);
+    }
 }
