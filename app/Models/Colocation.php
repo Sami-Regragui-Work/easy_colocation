ed7ce43abd6fb8d2063c3ae5be1285c4c1a74113
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-02-27 08:33:58 +0000

    feat(models): added Colocation relations

--- app/Models/Colocation.php
+++ app/Models/Colocation.php
@@ -22,4 +22,29 @@ class Colocation extends Model
     {
         return $this->belongsToMany(User::class, 'colocation_members')->withPivot(['role', 'left_at'])->withTimestamps()->orderBy('colocation_members.role', 'desc');
     }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    public function categories()
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    public function expenses()
+    {
+        return $this->hasMany(Expense::class);
+    }
+
+    public function invitations()
+    {
+        return $this->hasMany(Invitation::class);
+    }
+
+    public function settlements()
+    {
+        return $this->hasMany(Settlement::class);
+    }
 }
