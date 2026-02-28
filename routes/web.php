b26e3c9662e44017c762340cebbfcba0f249f13d
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-02-28 22:30:09 +0000

    feat(routes): added register, login and logout routes along with a fake dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::prefix('register')->group(function () {
+        Route::get('/', [RegisterController::class, 'show'])->name('register');
+        Route::post('/', [RegisterController::class, 'handle']);
+    });
+    Route::prefix('login')->group(function () {
+        Route::get('/', [LoginController::class, 'show'])->name('login');
+        Route::post('/', [LoginController::class, 'handle']);
+    });
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [LogoutController::class, 'handle'])->name('logout');
+    Route::get('/dashboard', fn() => view('dashboard'))->name('dashboard');
 });
